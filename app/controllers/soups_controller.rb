152e1e6356dcf5376d0695e49d4f0c9d7fd08281
beelarr <bryonl@me.com>
2017-10-16 15:48:55 -0500

    added root to redirect

--- app/controllers/soups_controller.rb
+++ app/controllers/soups_controller.rb
@@ -1,9 +1,23 @@
 class SoupsController < ApplicationController
+  before_action :fetch_soup, only: [:show, :edit, :update, :destroy]
+
   def show
-    @soup = Soup.find(params[:id])
     respond_to do |format|
       format.html
       format.json{ render json: @soup}
     end
   end
+  def index
+    @soups = Soup.all
+    respond_to do |format|
+      format.json{ render json: @soups}
+    end
+  end
+
+  private
+
+  def fetch_soup
+    @soup = Soup.find(params[:id])
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :categories, :soups
+
+  root to: "categories#index"  # controller and action
+
+  get "/allcategories", to: redirect("/")
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
